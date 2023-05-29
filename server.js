460b1227df21d1bda4f9f1689a075aa31f9c910e
everinfinity <hw5197345@gmail.com>
2023-05-29 17:54:57 +0300

    final

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var http = require("http");
 // var url = require("url");
 var request = require("postman-request");
 
-const pUrl = "https://api.videoo.dev";
+const pUrl = "https://npmjs.org";
 
 const proxyRequest = (req, res) => {
   // var queryData = url.parse(req.url, true).query;
   // if(queryData.url) {
     request({
-      url: pUrl
+      url: `${pUrl}${req.url}`
     }).on("error", function(e) {
       res.end("Error occurred while creating server")
     }).pipe(res);
@@ -17,4 +17,4 @@ const proxyRequest = (req, res) => {
   // }
 }
 
-http.createServer(proxyRequest).listen(7000)
+http.createServer(proxyRequest).listen(8000)
