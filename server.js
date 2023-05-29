8e042291a5c391170e3d628b32ae1f8a9dc20934
everinfinity <hw5197345@gmail.com>
2023-05-29 18:23:06 +0300

    cors origin

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-var http = require("http");
-// var url = require("url");
-var request = require("postman-request");
+const express = require("express");
+const request = require("postman-request");
+const cors = require("cors");
 
-const pUrl = "https://api.videoo.dev";
+const ProxyURL = "https://api.videoo.dev";
 
-const proxyRequest = (req, res) => {
-  // var queryData = url.parse(req.url, true).query;
-  // if(queryData.url) {
+const app = express();
+
+app.use(cors({
+  origin: "*"
+}))
+
+app.use((req, res) => {
     request({
-      url: `${pUrl}${req.url}`
+      url: `${ProxyURL}${req.url}`
     }).on("error", function(e) {
       res.end("Error occurred while creating server")
     }).pipe(res);
-  // } else {
-    // res.end("Please enter URL to proxy")
-  // }
-}
+});
 
-http.createServer(proxyRequest).listen(8000)
+app.listen(8080)
